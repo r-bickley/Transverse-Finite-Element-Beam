26201a5a5036523c044db36d43a202e7a11478b0
r-bickley <39009921+r-bickley@users.noreply.github.com>
2018-09-26 20:34:31 -0400

    Updated script produces correct natural frequencies

--- elemental_equations.m
+++ elemental_equations.m
@@ -1,25 +1,18 @@
-function [] = elemental_equations(E,I,L,rho)
+function [w_soln] = elemental_equations(E,I,L,rho)
 %Accepts the young's modulus, moment of inertia, and length
 %Elemental Stiffness Matrix:
 K = [12 6*L -12 6*L;6*L 4*L^2 -6*L 2*L^2;-12 -6*L 12 -6*L;6*L 2*L^2 -6*L 4*L^2];
 K = K*E*I./(L^3);
+K_soln = K(3:end,3:end);
 
 %Elemental Mass Matrix:
 M = [156 22*L 54 -13*L;22*L 4*L^2 13*L -3*L^2;54 13*L 156 -22*L; -13*L -3*L^2 -22*L 4*L^2];
 M = M*rho*L/420;
+M_soln = M(3:end,3:end);
 
 %Find the eigenvalues
-V = v_gen(K,M);
+V=eig(K_soln,M_soln);
 
 %Find the natural frequencies
-W = w_gen(V,4);
-
-disp("The elemental stiffness matrix is:")
-disp(K)
-disp("The elemental mass matrix is:")
-disp(M)
-disp("The corresponding eigenvalues are:")
-disp(V)
-disp("The natural frequencies are:")
-disp(W)
+w_soln = sqrt(V);
 end
